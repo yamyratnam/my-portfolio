b5dee8e9d538767df2b7873ce4c2b020ebf76746
yamyratnam <yamyratnam@gmail.com>
2023-11-18 20:56:30 +0530

    Commit 1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "lh3.googleusercontent.com",
+        },
+      ],
+    },
+    experimental: {
+      serverActions: true,
+    },
+  };
+  
+  module.exports = nextConfig;
